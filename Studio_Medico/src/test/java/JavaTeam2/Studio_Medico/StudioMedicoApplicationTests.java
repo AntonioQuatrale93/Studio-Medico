fcf02520cc0af48b42754760377b30986a5fc166
AntonioQuatrale93 <103583055+AntonioQuatrale93@users.noreply.github.com>
2022-07-15 17:14:32 +0200

    Add files via upload

---
+++ Studio_Medico/src/test/java/JavaTeam2/Studio_Medico/StudioMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaTeam2.Studio_Medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
