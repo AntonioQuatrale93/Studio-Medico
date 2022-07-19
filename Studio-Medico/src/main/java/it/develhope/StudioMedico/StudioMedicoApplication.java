81fc28ff2bdb9eac666c266d9bb35da542f7bb03
AntonioQuatrale93 <103583055+AntonioQuatrale93@users.noreply.github.com>
2022-07-19 16:56:55 +0200

    Add files via upload

---
+++ Studio-Medico/src/main/java/it/develhope/StudioMedico/StudioMedicoApplication.java
@@ -0,0 +1,13 @@
+package it.develhope.StudioMedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioMedicoApplication.class, args);
+	}
+
+}
---
+++ Studio-Medico/src/test/java/it/develhope/StudioMedico/StudioMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.StudioMedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
