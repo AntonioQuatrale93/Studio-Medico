0145b7bb5d96ca76e1272abaab157c82aa66e12a
matteo cimino <matteo02@gmail.com>
2022-08-28 22:10:18 +0200

    pazienteCrud

--- Studio-Medico/src/main/java/it/develhope/StudioMedico/dto/DoctorDto.java
+++ Studio-Medico/src/main/java/it/develhope/StudioMedico/dto/DoctorDto.java
@@ -4,15 +4,30 @@ package it.develhope.StudioMedico.dto;
  * Questa classe contiene solo gli attributi modificabili del dottore, ovvero indirizzo e numero di telefono
  */
 public class DoctorDto {
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String fiscalCode;
+
+    private String specialization;
     private String phoneNumber;
     private String address;
 
     public DoctorDto() {
     }
 
-    public DoctorDto(String phoneNumber, String address) {
+    public DoctorDto(String name,String surname,String email,String fiscalCode,String specialization,String phoneNumber, String address) {
         this.phoneNumber = phoneNumber;
         this.address = address;
+        this.name=name;
+        this.surname=surname;
+        this.email=email;
+        this.fiscalCode=fiscalCode;
+        this.specialization=specialization;
     }
 
     public String getPhoneNumber() {
@@ -29,5 +44,45 @@ public class DoctorDto {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFiscalCode() {
+        return fiscalCode;
+    }
+
+    public void setFiscalCode(String fiscalCode) {
+        this.fiscalCode = fiscalCode;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
     }
 }
