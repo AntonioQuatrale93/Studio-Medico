da2b8fc7c929e6c38021e804864f0279236803ec
matteo cimino <matteo02@gmail.com>
2022-08-28 22:09:52 +0200

    pazienteCrud

--- Studio-Medico/src/main/java/it/develhope/StudioMedico/dto/SecretaryDto.java
+++ Studio-Medico/src/main/java/it/develhope/StudioMedico/dto/SecretaryDto.java
@@ -2,15 +2,30 @@ package it.develhope.StudioMedico.dto;
 
 public class SecretaryDto {
 
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private int age;
+
+    private String fiscalCode;
+
     private String phoneNumber;
     private String address;
 
     public SecretaryDto() {
     }
 
-    public SecretaryDto(String phoneNumber, String address) {
+    public SecretaryDto(String name,String surname,String email,int age,String fiscalCode,String phoneNumber, String address) {
         this.phoneNumber = phoneNumber;
         this.address = address;
+        this.name=name;
+        this.surname=surname;
+        this.fiscalCode=fiscalCode;
+        this.email=email;
+        this.age=age;
     }
 
     public String getPhoneNumber() {
@@ -27,5 +42,45 @@ public class SecretaryDto {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFiscalCode() {
+        return fiscalCode;
+    }
+
+    public void setFiscalCode(String fiscalCode) {
+        this.fiscalCode = fiscalCode;
     }
 }
