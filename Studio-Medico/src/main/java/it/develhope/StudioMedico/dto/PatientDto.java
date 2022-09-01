ad87d482603820bf617faa13457d02406e45f060
Andrea <andreacolucci1994@gmail.com>
2022-09-01 16:43:43 +0200

    Updated DorctorDTO, PatientDto, SecretaryDto

--- Studio-Medico/src/main/java/it/develhope/StudioMedico/dto/PatientDto.java
+++ Studio-Medico/src/main/java/it/develhope/StudioMedico/dto/PatientDto.java
@@ -2,15 +2,65 @@ package it.develhope.StudioMedico.dto;
 
 public class PatientDto {
 
+    private String name;
+    private String surname;
+    private String fiscalCode;
+    private int age;
+    private String email;
     private String phoneNumber;
     private String address;
 
     public PatientDto() {
     }
 
-    public PatientDto(String phoneNumber, String address) {
+    public PatientDto(String name, String surname, String fiscalCode, int age, String email, String phoneNumber, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.fiscalCode = fiscalCode;
+        this.age = age;
+        this.email = email;
         this.phoneNumber = phoneNumber;
         this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFiscalCode() {
+        return fiscalCode;
+    }
+
+    public void setFiscalCode(String fiscalCode) {
+        this.fiscalCode = fiscalCode;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPhoneNumber() {
--- Studio-Medico/src/main/java/it/develhope/StudioMedico/dto/SecretaryDto.java
+++ Studio-Medico/src/main/java/it/develhope/StudioMedico/dto/SecretaryDto.java
@@ -2,15 +2,55 @@ package it.develhope.StudioMedico.dto;
 
 public class SecretaryDto {
 
+    private String name;
+    private String surname;
+    private String fiscalCode;
+    private String email;
     private String phoneNumber;
     private String address;
 
     public SecretaryDto() {
     }
 
-    public SecretaryDto(String phoneNumber, String address) {
+    public SecretaryDto(String name, String surname, String fiscalCode, String email, String phoneNumber, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.fiscalCode = fiscalCode;
+        this.email = email;
         this.phoneNumber = phoneNumber;
         this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFiscalCode() {
+        return fiscalCode;
+    }
+
+    public void setFiscalCode(String fiscalCode) {
+        this.fiscalCode = fiscalCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPhoneNumber() {
